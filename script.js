eaa5f5519cd974c1dbdb547ad7749c0a47efe2cd
nihalDevidas <117895185+nihalDevidas@users.noreply.github.com>
2023-07-17 17:49:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
     //Write your code here
+     const inputs = document.getElementsByTagName("input");
+     const inputArr = Array.from(inputs);
+	
+   alert(`${inputArr[0].value} ${inputArr[1].value}`);
 
+	
+	return false;
+	
 }
